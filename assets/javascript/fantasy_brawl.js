7fb261aab7563f1e49d769510fdfe5089050ded2
Christopher Biessener <cpbiessener@gmail.com>
2017-12-10 10:28:18 -0600

    skeleton FantasyBrawl object

--- assets/javascript/fantasy_brawl.js
+++ assets/javascript/fantasy_brawl.js
@@ -1,3 +1,11 @@
+// array of background images
+var backgrounds = [
+  "Fantasy-Landscape-Light-Mystical-Fairytale-2243769.jpg",
+  "castle-2596885_960_720.jpg",
+  "ireland-1971997_960_720.jpg",
+  "unicorn-2674743_960_720.jpg",
+];
+
 // Combatant Object Constructor
 function Combatant(hp, cur_hp, atk, ctr)
 {
@@ -7,7 +15,13 @@ function Combatant(hp, cur_hp, atk, ctr)
   this.counter = ctr;
 };
 
-var barbarian = new Combatant ( 200, 200, 10, 25 );
-var druid = new Combatant ( 100, 100, 12, 4 );
-var fighter = new Combatant ( 150, 150, 8, 15 );
-var survivor = new Combatant ( 250, 250, 6, 6 );
+// The Fantasy Brawl Game object
+var FantasyBrawl =
+{
+  // declare the 4 combatants
+  barbarian: new Combatant ( 200, 200, 10, 25 ),
+  druid: new Combatant ( 100, 100, 12, 4 ),
+  fighter: new Combatant ( 150, 150, 8, 15 ),
+  survivor: new Combatant ( 250, 250, 6, 6 ),
+};
+
